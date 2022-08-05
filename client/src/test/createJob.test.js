283537b860209e2d34da8bb96891fe76ed73e502
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-06 00:52:16 +0600

    create job: two tests added

--- client/src/test/createJob.test.js
+++ client/src/test/createJob.test.js
@@ -1,8 +1,8 @@
 import { expect } from "@jest/globals";
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import CreateJob from "../components/jobs/CreateJob";
-
 it('should clear fields when button clicked', async () => {
   const result = render(<CreateJob />); 
   const jobInput = result.container.querySelector("#title");
@@ -42,8 +42,51 @@ it('should clear fields when button clicked', async () => {
       descriptionInput: '',
       selectInput: ''
     }
-
-  console.log(val);
   
   expect(val).toEqual(expectedVal);
+
+})
+
+it('job input , company type, salary, location, should be type: text ', async () => {
+  const result = render(<CreateJob />); 
+  const jobInput = result.container.querySelector("#title");
+  const companyInput =  result.container.querySelector("#company");
+  const salaryInput = result.container.querySelector("#salary");
+  const locationInput =result.container.querySelector("#location");
+  const urlInput = result.container.querySelector("post_url");
+  const descriptionInput = result.container.querySelector("#description");
+  
+  const submit = screen.getByRole('button', {name: /Add Job/i});
+  //input
+  userEvent.type(jobInput, 'SE');
+  userEvent.type(companyInput, 'SEC');
+  userEvent.type(salaryInput, 'SES');
+  userEvent.type(locationInput, 'SEL');
+  userEvent.type(descriptionInput, 'SED'); 
+  userEvent.type(urlInput, 'https://URL.com');
+  
+  
+  await userEvent.click(submit);
+  expect(jobInput).toHaveAttribute('type', 'text');
+  expect(companyInput).toHaveAttribute('type', 'text');
+  expect(locationInput).toHaveAttribute('type', 'text');
+  
+
+})
+it('Job input, company should be required', async () => {
+  const result = render(<CreateJob />); 
+  const jobInput = result.container.querySelector("#title");
+  const companyInput =  result.container.querySelector("#company");
+  
+  const submit = screen.getByRole('button', {name: /Add Job/i});
+  //input
+  userEvent.type(jobInput, 'SE');
+  userEvent.type(companyInput, 'SEC');
+  
+  
+  
+  await userEvent.click(submit);
+  expect(jobInput).toHaveAttribute('required');
+  expect(companyInput).toHaveAttribute('required');
+
 })
