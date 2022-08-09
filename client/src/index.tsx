fdb56b2888eb9d86032f9affda54ddee3210d67e
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-09 23:41:58 +0600

    intialized tsc app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,7 @@ import './styles/formfields.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './components/context/AuthContext';
 
-const container = document.getElementById('root');
+const container: HTMLElement = document.getElementById('root') || new HTMLElement();
 const root = createRoot(container);
 
 root.render(
